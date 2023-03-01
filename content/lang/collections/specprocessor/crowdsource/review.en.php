a0fb132b2a4a330d83e6b783784d9635d4d45067
Katie Pearson <katelin.d.pearson24@gmail.com>
2023-02-28 16:15:57 -0800

    Lang tags for crowdsource/review.php

---
+++ content/lang/collections/specprocessor/crowdsource/review.en.php
@@ -0,0 +1,34 @@
+<?php
+/*
+------------------
+Language: English
+------------------
+*/
+
+$LANG['CROWDSOURCING_REVIEW'] = 'Crowdsourcing Review';
+$LANG['HOME'] = 'Home';
+$LANG['SCORE_BOARD'] = 'Score Board';
+$LANG['CONTROL_PANEL'] = 'Control Panel';
+$LANG['REVIEW_STATUS'] = 'Review Status';
+$LANG['ALL_RECORDS'] = 'All Records';
+$LANG['NOT_REVIEWED'] = 'Not Reviewed';
+$LANG['REVIEWED_APPROVED'] = 'Reviewed and Approved';
+$LANG['EDITOR'] = 'Editor';
+$LANG['ALL_EDITORS'] = 'All Editors';
+$LANG['SELECT_ALL'] = 'Select All';
+$LANG['POINTS'] = 'Points';
+$LANG['COMMENTS'] = 'Comments';
+$LANG['EDIT'] = 'Edit';
+$LANG['REVIEWED_APPROVED'] = 'Reviewed and Approved';
+$LANG['SUBMIT_REVIEWS'] = 'Submit Reviews';
+$LANG['SET_PROC_TO_REVIEWED'] = 'Set Processing Status to reviewed (unchecking will leave Processing Status as set by user for each record)';
+$LANG['SHOW_ADD_ACTIONS'] = 'Show Additional Actions';
+$LANG['SURE_CHANGE_STATUS'] = 'Are you sure you want to change review status? All points and review comments will be deleted.';
+$LANG['REMOVE_POINTS_CHANGE_NR'] = 'Remove points and change to Not Reviewed';
+$LANG['SURE_RESET_STATUS'] = 'Are you sure you want to reset status? Editor, points, and review comments will be deleted.';
+$LANG['MOVE_BACK_QUEUE'] = 'Move back into crowdsourcing queue as Open Records';
+$LANG['NO_RECS_THIS_USER'] = 'There are no more records to review for this user';
+$LANG['RETURN_TO'] = 'Return to';
+$LANG['NO_RECS'] = 'There are no records matching search criteria';
+
+?>
