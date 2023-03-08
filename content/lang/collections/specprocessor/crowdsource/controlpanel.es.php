866b1de12345b6b8e898a891c533a656c8a06098
Katie Pearson <katelin.d.pearson24@gmail.com>
2023-03-08 11:40:49 -0800

    Add Spanish translations (from Frank Bungartz)

---
+++ content/lang/collections/specprocessor/crowdsource/controlpanel.es.php
@@ -0,0 +1,40 @@
+<?php
+/*
+------------------
+Language: Espa&ntilde;ol
+------------------
+*/
+
+$LANG['CROWDSOURCE_EXPLAIN'] = 'Esta herramienta es para entregar y gestionar registros para el ingreso de datos por
+							el publico general. Por m&aacute;s informaci&oacute;n revisa a
+							<a href=" https://biokic.github.io/symbiota-docs/es/coll_manager/crowdsource/">
+							Documentaci&oacute;n de Symbiota para Crowdsourcing</a>';
+$LANG['EDIT_PROJECT'] = 'Editar el Proyecto';
+$LANG['GENERAL_INSTRUCTIONS'] = 'Instrucciones Generales';
+$LANG['TRAINING_URL'] = 'Url de Entrenamiento';
+$LANG['EDIT_CROWD_PROJ'] = 'Editar Proyecto de Crowdsourcing';
+$LANG['TOTAL_COUNTS'] = 'Puntaje Total';
+$LANG['RECS_IN_QUEUE'] = 'Puntaje Pendiente';
+$LANG['DEL_UNPROCESSED'] = 'Eliminar todos los registros de la lista no procesada';
+$LANG['PENDING_APPROVAL'] = 'Aprobaci&oacute;n Pendiente';
+$LANG['CLOSED_APPROVED'] = 'Cerrado (Aprobado)';
+$LANG['AVAILABLE_TO_ADD'] = 'Disponible para Agregar';
+$LANG['ADD_TO_QUEUE'] = ' A&ntilde;adir a la Lista de Espera';
+$LANG['CRITERIA'] = 'Criterios';
+$LANG['FAMILY'] = 'Familia';
+$LANG['GENUS_SP'] = 'Genero/Especie';
+$LANG['COUNTRY'] = 'Pa&iacute;s';
+$LANG['STATE_PROV'] = 'Estado/Provincia';
+$LANG['RECORD_LIMIT'] = 'L&iacute;mite de Registro ';
+$LANG['VOLUNTEERS'] = 'Voluntariados';
+$LANG['USER'] = 'Usuario';
+$LANG['SCORE'] = 'Puntaje';
+$LANG['PENDING_REVIEW'] = 'Revisi&oacute;n Pendiente';
+$LANG['APPROVED'] = 'Voluntariados';
+$LANG['REVIEW'] = 'Revisi&oacute;n';
+$LANG['NO_RECS_PROCESSED'] = 'Ning&uacute;n registro procesado';
+$LANG['APPROVED_EDITORS'] = 'Editores Aprobados';
+$LANG['SCORE_BOARD'] = 'Pizarra de Puntaje';
+$LANG['NO_COLLID'] = 'ERROR: n&uacute;mero de ID de la colecci&oacute;n no disponible';
+
+?>
