431e48a3d7458a1653411788fc3beeb15549c5aa
NikitaSalikov <86389284+NikitaSalikov@users.noreply.github.com>
2023-09-18 11:29:38 -0700

    508 compliance for collections/specprocessor/salix/salixhandler.php (#616)
    * added Doctype and lang, added labels, fixed fieldset, internationalized text
    
    * minor translation fixes
    
    * fixed label
    
    * fix missing title issue
    
    * fix issue with closing input element
    
    ---------
    
    Co-authored-by: Mark <mark.aaron.fisher@gmail.com>

---
+++ content/lang/collections/specprocessor/salix/salixhandler.en.php
@@ -0,0 +1,21 @@
+<?php
+/*
+------------------
+Language: English
+------------------
+*/
+
+$LANG['COLL_MGMT'] = 'Collection Management';
+$LANG['BREADCRUMB_SITEMAP'] = 'Sitemap';
+$LANG['SALIX_WRDST_MNGR'] = 'SALIX Wordstat Manager';
+$LANG['RETURN'] = 'Return to Main Menu';
+$LANG['ACTIONS'] = 'Actions';
+$LANG['RNDM_SELECT'] = 'Rebuild with randomly selected occurrences';
+$LANG['RECENTLY_ENTERED'] = 'Rebuild with most recently entered occurrences';
+$LANG['APPEND_LAST_BUILD'] = 'Append using occurrences entered since last build';
+$LANG['LIMIT_TO'] = 'Limit to';
+$LANG['UNIQUE_VALS'] = 'unique values per column';
+$LANG['BLD_TABLES'] = 'Build Wordstat Tables';
+$LANG['NOT_AUTH'] = 'You are not authorized to build Word Stats';
+
+?>
