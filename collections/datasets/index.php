e9a5a2c5f9f198c112c9ffce49f506cd87cf68b7
NikitaSalikov <86389284+NikitaSalikov@users.noreply.github.com>
2023-10-09 14:23:54 -0700

    508 compliance for collections/datasets/index.php (#646)
    * added Doctype and lang, added closing br tag, removed width attribute from textarea, internationalized text
    
    * added spaces for ternary operators

--- collections/datasets/index.php
+++ collections/datasets/index.php
@@ -1,15 +1,18 @@
+<!DOCTYPE html>
+
 <?php
 include_once('../../config/symbini.php');
 include_once($SERVER_ROOT.'/classes/OccurrenceDataset.php');
+include_once($SERVER_ROOT . '/content/lang/collections/datasets/index.' . $LANG_TAG . '.php');
 header("Content-Type: text/html; charset=".$CHARSET);
 
 if(!$SYMB_UID) header('Location: ../../profile/index.php?refurl=../collections/datasets/index.php?'.htmlspecialchars($_SERVER['QUERY_STRING'], ENT_QUOTES));
 
-$action = array_key_exists('submitaction',$_REQUEST)?$_REQUEST['submitaction']:'';
+$action = array_key_exists('submitaction',$_REQUEST) ? $_REQUEST['submitaction'] : '';
 
 //Sanitize input variables
 if($action && !preg_match('/^[a-zA-Z0-9\s_]+$/',$action)) $action = '';
-$isPublic = (isset($_POST['ispublic'])&&is_numeric($_POST['ispublic'])?1:0);
+$isPublic = (isset($_POST['ispublic'])&&is_numeric($_POST['ispublic']) ? 1 : 0);
 
 $datasetManager = new OccurrenceDataset();
 
@@ -32,10 +35,10 @@ elseif($action == 'addAllToDataset'){
 
 }
 ?>
-<html>
+<html lang="<?php echo $LANG_TAG ?>">
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=<?php echo $CHARSET;?>">
-		<title><?php echo $DEFAULT_TITLE; ?> Occurrence Dataset Manager</title>
+		<title><?php echo $DEFAULT_TITLE; ?> <?php echo (isset($LANG['OCC_DAT_MNG']) ? $LANG['OCC_DAT_MNG'] : 'Occurrence Dataset Manager') ?> </title>
 		<link href="<?php echo htmlspecialchars($CSS_BASE_PATH, HTML_SPECIAL_CHARS_FLAGS); ?>/jquery-ui.css" type="text/css" rel="stylesheet">
 		<?php
 		include_once($SERVER_ROOT.'/includes/head.php');
@@ -93,13 +96,13 @@ elseif($action == 'addAllToDataset'){
 	</head>
 	<body>
 	<?php
-	$displayLeftMenu = (isset($collections_datasets_indexMenu)?$collections_datasets_indexMenu:false);
+	$displayLeftMenu = (isset($collections_datasets_indexMenu) ? $collections_datasets_indexMenu : false);
 	include($SERVER_ROOT.'/includes/header.php');
 	?>
 	<div class='navpath'>
-		<a href='../../index.php'>Home</a> &gt;&gt;
-		<a href="../../profile/viewprofile.php?tabindex=1">My Profile</a> &gt;&gt;
-		<a href="index.php"><b>Dataset Listing</b></a>
+		<a href='../../index.php'> <?php echo (isset($LANG['HOME']) ? $LANG['HOME'] : 'Home') ?> </a> &gt;&gt;
+		<a href="../../profile/viewprofile.php?tabindex=1"> <?php echo (isset($LANG['MY_PROFILE']) ? $LANG['MY_PROFILE'] : 'My Profile') ?> </a> &gt;&gt;
+		<a href="index.php"><b> <?php echo (isset($LANG['DATLIST']) ? $LANG['DATLIST'] : 'Dataset Listing') ?> </b></a>
 	</div>
 	<!-- This is inner text! -->
 	<div id="innertext">
@@ -116,35 +119,35 @@ elseif($action == 'addAllToDataset'){
 		$dataSetArr = $datasetManager->getDatasetArr();
 		?>
 		<div>
-		<div style="float:right;margin:10px;" title="Create New Dataset" >
-	 		<a href="#" onclick="toggle('adddiv');return false;"><img src="../../images/add.png" style="width:14px;" /></a>
+		<div style="float:right;margin:10px;" title="<?php echo (isset($LANG['CRT_NEW_DAT']) ? $LANG['CRT_NEW_DAT'] : 'Create New Dataset') ?>" >
+	 		<a href="#" onclick="toggle('adddiv');return false;"><img src="../../images/add.png"  style="width:14px;" alt="<?php echo (isset($LANG['ADD_BUTTON']) ? $LANG['ADD_BUTTON'] : 'Add Button') ?>"/> </a>
 		</div>
-		<h2>Occurrence Dataset Management</h2>
-		<div>These tools will allow you to define and manage dataset profiles. Once a profile is created, you can link occurrence records via the occurrence search and display pages.</div>
+		<h2> <?php echo (isset($LANG['OCC_DAT_MNG']) ? $LANG['OCC_DAT_MNG'] : 'Occurrence Dataset Manager') ?> </h2>
+		<div> <?php echo (isset($LANG['TOOL_DESCR']) ? $LANG['TOOL_DESCR'] : 'These tools will allow you to define and manage dataset profiles. Once a profile is created, you can link occurrence records via the occurrence search and display pages.') ?> </div>
 		<div id=adddiv style="display:none">
 			<fieldset>
-				<legend><b>Create New Dataset</b></legend>
+				<legend><b> <?php echo (isset($LANG['CRT_NEW_DAT']) ? $LANG['CRT_NEW_DAT'] : 'Create New Dataset') ?> </b></legend>
 				<form name="adminform" action="index.php" method="post" onsubmit="return validateEditForm(this)">
 					<div>
-						<p><b>Name</b></p>
+						<p><b> <?php echo (isset($LANG['NAME']) ? $LANG['NAME'] : 'Name') ?> </b></p>
 						<input name="name" type="text" style="width:90%" />
 					</div>
           <div>
             <p>
               <input type="checkbox" name="ispublic" id="ispublic" value="1" />
-            <b>Publicly Visible</b>
+            <b> <?php echo (isset($LANG['PUB_VIS']) ? $LANG['PUB_VIS'] : 'Publicly Visible') ?> </b>
             </p>
           </div>
 					<div>
-						<p><b>Notes (Internal usage, not displayed publicly)</b></p>
+						<p><b> <?php echo (isset($LANG['NOTES']) ? $LANG['NOTES'] : 'Notes (Internal usage, not displayed publicly)') ?></b></p>
 						<input name="notes" type="text" style="width:90%;" />
 					</div>
           <div>
-            <p><b>Description (Displayed publicly)</p>
-            <textarea name="description" id="description" cols="100" rows="10" width="90%"></textarea>
+            <p><b> <?php echo (isset($LANG['DESCR']) ? $LANG['DESCR'] : 'Description (Displayed publicly)') ?> </b> </p>
+            <textarea name="description" id="description" cols="100" rows="10"></textarea>
           </div>
 					<div style="margin:15px">
-						<button name="submitaction" type="submit" value="createNewDataset">Create New Dataset</button>
+						<button name="submitaction" type="submit" value="createNewDataset"> <?php echo (isset($LANG['CRT_NEW_DAT']) ? $LANG['CRT_NEW_DAT'] : 'Create New Dataset') ?> </button>
 					</div>
 				</form>
 			</fieldset>
@@ -153,7 +156,7 @@ elseif($action == 'addAllToDataset'){
 		if($dataSetArr){
 			?>
 			<fieldset>
-				<legend><b>Owned by You</b></legend>
+				<legend><b> <?php echo (isset($LANG['OWNED']) ? $LANG['OWNED'] : 'Owned by You') ?> </b></legend>
 				<?php
 				if(array_key_exists('owner',$dataSetArr)){
 					$ownerArr = $dataSetArr['owner'];
@@ -162,7 +165,7 @@ elseif($action == 'addAllToDataset'){
 						?>
 						<div class="dataset-item">
 							<div>
-								<a href="datasetmanager.php?datasetid=<?php echo $dsid; ?>" title="Manage and edit dataset">
+								<a href="datasetmanager.php?datasetid=<?php echo $dsid; ?>" title="<?php echo (isset($LANG['MNG_EDIT']) ? $LANG['MNG_EDIT'] : 'Manage and edit dataset') ?>">
 									<?php
 									echo '<b>'.$dsArr['name'].' (#'.$dsid.')</b>';
 									?>
@@ -170,7 +173,7 @@ elseif($action == 'addAllToDataset'){
 							</div>
 							<div style="margin-left:15px;">
 								<?php
-								echo ($dsArr['notes']?'<div>'.$dsArr['notes'].'</div>':'');
+								echo ($dsArr['notes'] ? '<div>'.$dsArr['notes'].'</div>' : '');
 								echo '<div>Created: '.$dsArr["ts"].'</div>';
 								?>
 							</div>
@@ -179,12 +182,12 @@ elseif($action == 'addAllToDataset'){
 					}
 				}
 				else{
-					echo '<div style="font-weight:bold;">There are no datasets owned by you</div>';
+					echo '<div style="font-weight:bold;">' . (isset($LANG['NO_OWNED']) ? $LANG['NO_OWNED'] : 'There are no datasets owned by you') . '</div>';
 				}
 				?>
 			</fieldset>
 			<fieldset>
-				<legend>Shared with You</legend>
+				<legend><?php echo (isset($LANG['SHARED']) ? $LANG['SHARED'] : 'Shared with You') ?> </legend>
 				<?php
 				if(array_key_exists('other',$dataSetArr)){
 					$otherArr = $dataSetArr['other'];
@@ -209,7 +212,7 @@ elseif($action == 'addAllToDataset'){
 						<?php
 					}
 				}
-				else echo '<div style="font-weight:bold;">There are no datasets shared with you</div>';
+				else echo '<div style="font-weight:bold;">' . (isset($LANG['NO_SHARED']) ? $LANG['NO_SHARED'] : 'There are no datasets shared with you') . '</div>';
 				?>
 			</fieldset>
 			<?php
@@ -217,8 +220,8 @@ elseif($action == 'addAllToDataset'){
 		else{
 			?>
 			<div style="margin:20px">
-				<div style="font-weight:bold">There are no datasets associated to your login</div>
-				<div style="margin-top:15px"><a href="#" onclick="toggle('adddiv');">Create a New Dataset</a></div>
+				<div style="font-weight:bold"> <?php echo (isset($LANG['NO_LOGIN']) ? $LANG['NO_LOGIN'] : 'There are no datasets associated to your login') ?> </div>
+				<div style="margin-top:15px"><a href="#" onclick="toggle('adddiv');"> <?php echo (isset($LANG['CRT_NEW_DAT']) ? $LANG['CRT_NEW_DAT'] : 'Create New Dataset') ?> </a></div>
 			</div>
 			<?php
 		}
