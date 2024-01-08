d0e0dcbceb27c288822dff6553d5948c019861de
NikitaSalikov <86389284+NikitaSalikov@users.noreply.github.com>
2024-01-08 14:37:47 -0700

    508 for collections/reports/labelmanager.php (#840)
    * added doctype, lang, and labels, fixed minor issues, internationalized
    
    * add another label to the select element, and change aria-label per TVP feedback
    
    ---------
    
    Co-authored-by: Mark <mark.aaron.fisher@gmail.com>

---
+++ content/lang/collections/reports/labelmanager.en.php
@@ -0,0 +1,73 @@
+<?php
+/*
+------------------
+Language: English
+------------------
+*/
+
+$LANG['SPEC_LABEL_MANAGER'] = 'Specimen Label Manager';
+$LANG['PERS_MANAG_MENU'] = 'Personal Management Menu';
+$LANG['CONTACT_ADMIN'] = 'Please contact the site administrator to make temp/report folder writable in order to export to docx files.';
+$LANG['DEF_SPEC_REC'] = 'Define Specimen Recordset';
+$LANG['SCI_NAME'] = 'Scientific Name: ';
+$LANG['FULL_NAME'] = 'Full or last name of collector as entered in database.';
+$LANG['COLLECTOR'] = 'Collector:';
+$LANG['SEPARATE_TERMS'] = 'Separate multiple terms by comma and ranges by \' - \' (space before and after dash required), e.g.: 3542,3602,3700 - 3750';
+$LANG['REC_NUM'] = 'Record Number(s):';
+$LANG['CAT_NUM'] = 'Catalog Number(s):';
+$LANG['ENTER_BY'] = 'Entered by:';
+$LANG['LOG_NAME'] = 'login name of data entry person';
+$LANG['DATE_RANGE'] = 'Date range:';
+$LANG['TO'] = 'to';
+$LANG['DATE_ENTER'] = 'Date Entered';
+$LANG['DATE_MOD'] = 'Date Modified';
+$LANG['DATE_COLL'] = 'Date Collected';
+$LANG['LABEL_PROJ'] = 'Label Projects:';
+$LANG['ALL_PROJ'] = 'All Projects';
+$LANG['SEARCH_OUT'] = 'Search outside user profile';
+$LANG['SEARCH_IN'] = 'Search within all collections';
+$LANG['FILT_SPEC_REC'] = 'Filter Specimen Records';
+$LANG['SPEC_LIM'] = '* Specimen return is limited to 1000 records';
+$LANG['QTY'] = 'Qty';
+$LANG['LOCALITY'] = 'Locality';
+$LANG['LABEL_QTY'] = 'Label quantity';
+$LANG['LABEL_PRINT'] = 'Label Printing';
+$LANG['LABEL_PROFILE'] = 'Label Profiles:';
+$LANG['SEL_LABEL_FORMAT'] = 'Select a Label Format';
+$LANG['LABEL_NOT_SET'] = 'label profiles have not yet been set within portal';
+$LANG['HEAD_PREFIX'] = 'Heading Prefix:';
+$LANG['E_G_PLANTS'] = '(e.g. Plants of, Insects of, Vertebrates of)';
+$LANG['HEAD_MID'] = 'Heading Mid-Section:';
+$LANG['COUNTRY'] = 'Country';
+$LANG['STATE'] = 'State';
+$LANG['COUNTY'] = 'County';
+$LANG['FAMILY'] = 'Family';
+$LANG['BLANK'] = 'Blank';
+$LANG['HEAD_SUFF'] = 'Heading Suffix:';
+$LANG['FOOTER'] = 'Footer:';
+$LANG['PRINT_AUTH'] = 'Print species authors for infraspecific taxa';
+$LANG['PRINT_CAT_NUM'] = 'Print Catalog Numbers';
+$LANG['INCL_BARCODE'] = 'Include barcode of Catalog Number';
+$LANG['PRINT_BARCODE'] = 'Print only Barcode';
+$LANG['LABEL_TYPE'] = 'Label Type:';
+$LANG['COLL_PAGE'] = 'columns per page';
+$LANG['PACKET_LABEL'] = 'Packet labels';
+$LANG['CONTACT_ADMIN'] = 'Browser based label printing has not been activated within the portal. Contact Portal Manager to activate this feature.';
+$LANG['NOTE'] = 'Note:';
+$LANG['NOTE_1'] = 'Currently, Word (DOCX) output only generates the old static label format.';
+$LANG['NOTE_2'] = 'Output of variable Label Formats (pulldown options) as a Word document is not yet supported.';
+$LANG['NOTE_3'] = 'A possible work around is to print labels as PDF and then convert to a Word doc using Adobe tools.';
+$LANG['NOTE_4'] = 'Another alternatively, is to output the data as CSV and then setup a Mail Merge Word document.';
+$LANG['NO_DATA'] = 'Query returned no data!';
+$LANG['NO_PERM'] = 'You do not have permissions to print labels for this collection.
+            Please contact the site administrator to obtain the necessary permissions.';
+$LANG['PRINT_BROWSER'] = 'Print in Browser';
+$LANG['EXP_CSV'] = 'Export to CSV';
+$LANG['EXP_DOCX'] = 'Export to DOCX';
+$LANG['ALERT_DATE'] = 'Date entered must follow the format YYYY-MM-DD';
+$LANG['ALERT_SPEC'] = 'At least one specimen checkbox needs to be selected with a label quantity greater than 0';
+$LANG['ALERT_LABEL'] = 'Please select a Label Format Profile';
+$LANG['ALERT_PACKET_LABEL'] = 'Packet labels are not yet available as a Word document';
+$LANG['TYPE_OF_DATE'] = 'Type of date';
+
+?>
