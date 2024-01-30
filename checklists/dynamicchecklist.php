18243b4d4e811daf6249b1fea238f63630ede738
Edward Gilbert <egbot@asu.edu>
2024-01-30 16:22:39 -0700

    Bug fix
    - Sanitize lat and long values within float filter allowing fractions
    - Fix previous bug within language file import

--- checklists/dynamicchecklist.php
+++ checklists/dynamicchecklist.php
@@ -1,11 +1,11 @@
 <?php
 include_once('../config/symbini.php');
 include_once($SERVER_ROOT . '/classes/DynamicChecklistManager.php');
-include_once($SERVER_ROOT . '/content/lang/checklists/checklist . ' . $LANG_TAG . '.php');
-header("Content-Type: text/html; charset=".$CHARSET);
+include_once($SERVER_ROOT . '/content/lang/checklists/checklist.' . $LANG_TAG . '.php');
+header('Content-Type: text/html; charset=' . $CHARSET);
 
-$lat = filter_var($_POST['lat'], FILTER_SANITIZE_NUMBER_INT);
-$lng = filter_var($_POST['lng'], FILTER_SANITIZE_NUMBER_INT);
+$lat = filter_var($_POST['lat'], FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
+$lng = filter_var($_POST['lng'], FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
 $radius = filter_var($_POST['radius'], FILTER_SANITIZE_NUMBER_INT);
 $radiusUnits = $_POST['radiusunits'];
 $dynamicRadius = isset($DYN_CHECKLIST_RADIUS) ?? 10;
@@ -15,14 +15,13 @@ $interface = $_POST['interface'];
 
 //sanitation
 if($radiusUnits != 'mi') $radiusUnits == 'km';
-
+echo 'lat: '.$lat.'; lng: '. $lng . '; radius: '. $radius;
 $dynClManager = new DynamicChecklistManager();
 
 if($taxa && !$tid) $tid = $dynClManager->getTid($taxa);
 $dynClid = 0;
 if($radius) $dynClid = $dynClManager->createChecklist($lat, $lng, $radius, $radiusUnits, $tid);
 else $dynClid = $dynClManager->createDynamicChecklist($lat, $lng, $dynamicRadius, $tid);
-
 if($dynClid){
 	if($interface == "key"){
 		header("Location: ".$CLIENT_ROOT."/ident/key.php?dynclid=".$dynClid."&taxon=All Species");
