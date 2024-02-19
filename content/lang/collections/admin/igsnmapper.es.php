fbda7e9ee3f352ed7ffff56dea31d823bbdbcaee
NikitaSalikov <86389284+NikitaSalikov@users.noreply.github.com>
2024-02-19 10:25:28 -0700

    Lang tag for collections/admin/igsnmapper.php (#943)
    * internationalized text
    
    * fixed typo, internationalized text
    
    * removed template lang tag
    
    * fixed typo in spanish file

---
+++ content/lang/collections/admin/igsnmapper.es.php
@@ -0,0 +1,44 @@
+<?php
+/*
+------------------
+Language: Español (Spanish)
+Translated by: Google Translate
+Date Translated: 2024-02-12
+------------------
+*/
+
+$LANG['IGSN_GUID_MAPPER'] = 'Asignador GUID IGSN';
+$LANG['ENTER_VALID'] = 'Por favor, introduzca un nombre de usuario y una contraseña válidos de SESAR';
+$LANG['MUST_BE_EMAIL'] = 'El nombre de usuario de SESAR debe ser una dirección de correo electrónico';
+$LANG['SELECT_REG'] = 'Seleccione un método de registro';
+$LANG['IGSN_NOT_GENERATED'] = 'Generación de IGSN interna seleccionada pero semilla de IGSN no generada (contacte al administrador)';
+$LANG['COLL_MANAGE'] = 'Gestión de colecciones';
+$LANG['SESAR_API_WEB'] = 'Servicios web SESAR API';
+$LANG['IGSN_GUID_MANAGE'] = 'Gestión de GUID IGSN';
+$LANG['IGSN_MAPPER'] = 'Asignador IGSN';
+$LANG['ERROR_PANEL'] = 'Panel de errores';
+$LANG['DEV_MODE'] = 'En modo de desarrollo';
+$LANG['ACTION_PANEL'] = 'Panel de acciones';
+$LANG['IGSN_REG_PANEL'] = 'Panel de control de registro IGSN';
+$LANG['REG_IGSN'] = 'Registrar IGSN mediante el envío manual de un archivo de datos o automáticamente';
+$LANG['USERNAME'] = 'Nombre de usuario:';
+$LANG['CRED_VALID'] = '¡Credenciales válidas!';
+$LANG['CRED_NOT_VALID'] = 'Credenciales no válidas';
+$LANG['PASSWORD'] = 'Contraseña:';
+$LANG['VALIDATE_CRED'] = 'Validar credenciales';
+$LANG['OCC_WITHOUT_GUID'] = 'Ocurrencias sin GUID:';
+$LANG['IGSN_NAMESPACE'] = 'Espacio de nombres IGSN:';
+$LANG['IGSN_GEN_METHOD'] = 'Método de generación de IGSN:';
+$LANG['IGSN_SEED'] = 'Semilla IGSN:';
+$LANG['REG_METHOD'] = 'Método de registro:';
+$LANG['SELECT_METHOD'] = 'Seleccionar método';
+$LANG['SESAR_API'] = 'API SESAR';
+$LANG['EXPORT_XML'] = 'Exportar XML';
+$LANG['NUM_GEN'] = 'Número de identificadores a generar';
+$LANG['LEAVE_BLANK'] = '(dejar en blanco para procesar todas las muestras)';
+$LANG['POPULATE_COLL_GUID'] = 'Rellenar GUID de colección';
+$LANG['FATAL_ERROR'] = 'ERROR GRAVE:';
+$LANG['NAMESPACE_NOT_SET'] = 'el espacio de nombres y el método de generación no están configurados';
+$LANG['NOT_AUTH'] = 'No está autorizado a acceder a esta página o no se ha configurado el identificador de colección';
+
+?>
