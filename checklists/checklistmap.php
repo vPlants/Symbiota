335844e4636400490f6ffb6b9e627124010ba751
Ramisa Zaman <129628108+RamisaZaman@users.noreply.github.com>
2024-03-20 12:24:35 -0700

    508 for checklists/checklistmap.php (#1027)
    * 508 for checklists/checklistmap.php
    
    * implement more TVP changes in checklistmap.php
    
    * removing uneeded comments for TODO(Logan) since this task was finished
    
    ---------
    
    Co-authored-by: Mark <mark.aaron.fisher@gmail.com>
    Co-authored-by: MuchQuak <loganpwilt@gmail.com>

--- checklists/checklistmap.php
+++ checklists/checklistmap.php
@@ -34,246 +34,247 @@ $coordJson = json_encode($coords);
 $metaJson = json_encode($clMeta);
 
 ?>
-<html>
-<head>
-	<title><?php echo $DEFAULT_TITLE . ' - ' . $LANG['COORD_MAP']; ?></title>
-	<?php
-	include_once($SERVER_ROOT.'/includes/googleanalytics.php');
-	include_once($SERVER_ROOT.'/includes/leafletMap.php');
-	include_once($SERVER_ROOT.'/includes/googleMap.php');
-	?>
+<!DOCTYPE html>
+<html lang="<?php echo $LANG_TAG ?>">
+   <head>
+      <title><?php echo $DEFAULT_TITLE . ' - ' . $LANG['COORD_MAP']; ?></title>
+      <?php
+      include_once($SERVER_ROOT.'/includes/googleanalytics.php');
+      include_once($SERVER_ROOT.'/includes/leafletMap.php');
+      include_once($SERVER_ROOT.'/includes/googleMap.php');
+      ?>
 
-   <script src="<?php echo $CLIENT_ROOT?>/js/symb/wktpolygontools.js" type="text/javascript"></script>
-   <script src="<?php echo $CLIENT_ROOT?>/js/symb/MapShapeHelper.js" type="text/javascript"></script>
-
-	<script type="text/javascript">
-      var map;
-      var puWin;
-      let occurCoords = [];
-      let clMeta;
-      let cl_footprint_shape;
-
-      function leafletInit() {
-			var dmOptions = {
-				zoom: 3,
-				center: [41,-95],
-         };
-         map = new LeafletMap("map_canvas", dmOptions);
-         const leafletSmallPin = img => L.icon({
-            iconUrl: img,
-            iconSize:     [12, 20],
-            iconAnchor:   [6, 20],
-            popupAnchor:  [0, -12],
-            tooltipAnchor:  [0, -12]
-         });
-
-         var symb_occur = leafletSmallPin('../images/google/smpin_red.png')
-         var symb = leafletSmallPin('../images/google/smpin_blue.png')
-         var inat_icon = leafletSmallPin('../images/google/smpin_green.png')
-
-         let markers = []
-         for(let coord of occurCoords) {
-            let marker = L.marker([coord.lat, coord.lng], {
-               icon: coord.occid? symb_occur: symb,
-            })
-            .bindTooltip(`<div style="font-size:1.2rem">${coord.notes}</div>`)
-            .on('click', () => openIndPU(coord.occid));
-            markers.push(marker);
+      <script src="<?php echo $CLIENT_ROOT?>/js/symb/wktpolygontools.js" type="text/javascript"></script>
+      <script src="<?php echo $CLIENT_ROOT?>/js/symb/MapShapeHelper.js" type="text/javascript"></script>
+      <style>
+			.for-screen-reader{ 
+            position: absolute;
+            left: -10000px;
          }
-         let markerGroup = new L.FeatureGroup(markers);
+		</style>
 
-         map.enableDrawing({...map.DEFAULT_DRAW_OPTIONS, control: false});
-         if(cl_footprint_shape) map.drawShape(cl_footprint_shape);
+      <script type="text/javascript">
+         var map;
+         var puWin;
+         let occurCoords = [];
+         let clMeta;
+         let cl_footprint_shape;
 
-         markerGroup.addTo(map.mapLayer)
-
-         map.mapLayer.fitBounds(markerGroup.getBounds());
-
-         //Only for inaturalist for now
-         if(clMeta.dynamicProperties && clMeta.dynamicProperties.externalserviceid) {
-            //TODO (Logan) Only grabs 200 records for now setup multi request
-            //fill in. Note this must be throttled to be less than 100 requests
-            //per minute for iNaturalist guidelines
-            getInatProjectOccurrences(clMeta.dynamicProperties.externalserviceid).then(res => {
-               let inat_markers = [];
-               for(let occur of res.results) {
-                  if(occur.geojson && occur.geojson.type === "Point") {
-                     let marker = L.marker(occur.geojson.coordinates.reverse(), {
-                        icon: inat_icon,
-                     })
-                     .bindTooltip(`<div style="font-size:1.2rem">iNaturalist-${occur.id}</div>`)
-                     .on('click', () => window.open(occur.uri, '_blank'));
-
-                     inat_markers.push(marker);
-                  }
-               }
-
-               let inat_markerGroup = new L.FeatureGroup(inat_markers);
-               inat_markerGroup.addTo(map.mapLayer)
+         function leafletInit() {
+            var dmOptions = {
+               zoom: 3,
+               center: [41,-95],
+            };
+            map = new LeafletMap("map_canvas", dmOptions);
+            const leafletSmallPin = img => L.icon({
+               iconUrl: img,
+               iconSize:     [12, 20],
+               iconAnchor:   [6, 20],
+               popupAnchor:  [0, -12],
+               tooltipAnchor:  [0, -12]
             });
-         }
 
-      }
+            var symb_occur = leafletSmallPin('../images/google/smpin_red.png')
+            var symb = leafletSmallPin('../images/google/smpin_blue.png')
+            var inat_icon = leafletSmallPin('../images/google/smpin_green.png')
 
-      function googleInit() {
-         var vIcon = new google.maps.MarkerImage("../images/google/smpin_red.png");
-         var pIcon = new google.maps.MarkerImage("../images/google/smpin_blue.png");
-         var inatIcon = new google.maps.MarkerImage("../images/google/smpin_green.png");
+            let markers = []
+            for(let coord of occurCoords) {
+               let marker = L.marker([coord.lat, coord.lng], {
+                  icon: coord.occid? symb_occur: symb,
+               })
+               .bindTooltip(`<div style="font-size:1.2rem">${coord.notes}</div>`)
+               .on('click', () => openIndPU(coord.occid));
+               markers.push(marker);
+            }
+            let markerGroup = new L.FeatureGroup(markers);
 
-			var dmOptions = {
-				zoom: 3,
-				center: new google.maps.LatLng(41,-95),
-				mapTypeId: google.maps.MapTypeId.TERRAIN,
-				scaleControl: true
-         };
-         map = new GoogleMap("map_canvas", dmOptions);
+            map.enableDrawing({...map.DEFAULT_DRAW_OPTIONS, control: false});
+            if(cl_footprint_shape) map.drawShape(cl_footprint_shape);
 
-			var bounds = new google.maps.LatLngBounds();
+            markerGroup.addTo(map.mapLayer)
 
-         for(let coord of occurCoords) {
-            let marker = new google.maps.Marker({
-					position: new google.maps.LatLng(coord.lat, coord.lng),
-               title: coord.notes,
-               icon: coord.occid? vIcon: pIcon,
-               map: map.mapLayer,
-					zIndex: google.maps.Marker.MAX_ZINDEX
+            map.mapLayer.fitBounds(markerGroup.getBounds());
+
+            //Only for inaturalist for now
+            if(clMeta.dynamicProperties && clMeta.dynamicProperties.externalserviceid) {
+               getInatProjectOccurrences(clMeta.dynamicProperties.externalserviceid).then(res => {
+                  let inat_markers = [];
+                  for(let occur of res.results) {
+                     if(occur.geojson && occur.geojson.type === "Point") {
+                        let marker = L.marker(occur.geojson.coordinates.reverse(), {
+                           icon: inat_icon,
+                        })
+                        .bindTooltip(`<div style="font-size:1.2rem">iNaturalist-${occur.id}</div>`)
+                        .on('click', () => window.open(occur.uri, '_blank'));
+
+                        inat_markers.push(marker);
+                     }
+                  }
+
+                  let inat_markerGroup = new L.FeatureGroup(inat_markers);
+                  inat_markerGroup.addTo(map.mapLayer)
                });
-            bounds.extend(marker.getPosition());
-
-            google.maps.event.addListener(marker, 'click', function() {
-               openIndPU(coord.occid)
-            })
+            }
          }
+         function googleInit() {
+            var vIcon = new google.maps.MarkerImage("../images/google/smpin_red.png");
+            var pIcon = new google.maps.MarkerImage("../images/google/smpin_blue.png");
+            var inatIcon = new google.maps.MarkerImage("../images/google/smpin_green.png");
 
-         map.mapLayer.fitBounds(bounds);
+            var dmOptions = {
+               zoom: 3,
+               center: new google.maps.LatLng(41,-95),
+               mapTypeId: google.maps.MapTypeId.TERRAIN,
+               scaleControl: true
+            };
+            map = new GoogleMap("map_canvas", dmOptions);
 
-         map.enableDrawing({...map.DEFAULT_DRAW_OPTIONS, polygon: false, rectangle: false, circle: false});
-         if(cl_footprint_shape) map.drawShape(cl_footprint_shape);
+            var bounds = new google.maps.LatLngBounds();
 
-         //Only for inaturalist for now
-         if(clMeta.dynamicProperties && clMeta.dynamicProperties.externalserviceid) {
-            //TODO (Logan) Only grabs 200 records for now setup multi request
-            //fill in. Note this must be throttled to be less than 100 requests
-            //per minute for iNaturalist guidelines
-            getInatProjectOccurrences(clMeta.dynamicProperties.externalserviceid).then(res => {
-               for(let occur of res.results) {
-                  if(occur.geojson && occur.geojson.type === "Point") {
-                     let marker = new google.maps.Marker({
-                        position: new google.maps.LatLng(occur.geojson.coordinates[1], occur.geojson.coordinates[0]),
-                        title: occur.uri,
-                        title: "iNaturalist-" + occur.id,
-                        icon: inatIcon,
-                        map: map.mapLayer,
-                        zIndex: google.maps.Marker.MAX_ZINDEX
-                     });
+            for(let coord of occurCoords) {
+               let marker = new google.maps.Marker({
+                  position: new google.maps.LatLng(coord.lat, coord.lng),
+                  title: coord.notes,
+                  icon: coord.occid? vIcon: pIcon,
+                  map: map.mapLayer,
+                  zIndex: google.maps.Marker.MAX_ZINDEX
+                  });
+               bounds.extend(marker.getPosition());
 
-                     google.maps.event.addListener(marker, 'click', () => {
-                        window.open(occur.uri, '_blank');
-                     });
+               google.maps.event.addListener(marker, 'click', function() {
+                  openIndPU(coord.occid)
+               })
+            }
+
+            map.mapLayer.fitBounds(bounds);
+
+            map.enableDrawing({...map.DEFAULT_DRAW_OPTIONS, polygon: false, rectangle: false, circle: false});
+            if(cl_footprint_shape) map.drawShape(cl_footprint_shape);
+
+            //Only for inaturalist for now
+            if(clMeta.dynamicProperties && clMeta.dynamicProperties.externalserviceid) {
+               getInatProjectOccurrences(clMeta.dynamicProperties.externalserviceid).then(res => {
+                  for(let occur of res.results) {
+                     if(occur.geojson && occur.geojson.type === "Point") {
+                        let marker = new google.maps.Marker({
+                           position: new google.maps.LatLng(occur.geojson.coordinates[1], occur.geojson.coordinates[0]),
+                           title: occur.uri,
+                           title: "iNaturalist-" + occur.id,
+                           icon: inatIcon,
+                           map: map.mapLayer,
+                           zIndex: google.maps.Marker.MAX_ZINDEX
+                        });
+
+                        google.maps.event.addListener(marker, 'click', () => {
+                           window.open(occur.uri, '_blank');
+                        });
+                     }
                   }
+               });
+            }
+         }
+
+         function parseNested(str) {
+            try {
+               return JSON.parse(str, (_, val) => {
+                     if (typeof val === 'string')
+                        return parseNested(val)
+                     return val
+               })
+            } catch (e) {
+               return str
+            }
+         }
+
+         // Note Need to Throttle to < 100 requests per minute as per iNaturalist API guidelines
+         async function getInatProjectOccurrences(inat_proj_id, page = 1) {
+            let url = `https://api.inaturalist.org/v1/observations?project_id=${inat_proj_id}&geo=true&page=${page}&mappable=true&per_page=200`;
+            let response = await fetch(url, {
+               method: "GET",
+               headers: {
+                  "Content-Type": "application/json",
                }
-            });
-         }
-      }
+            })
+            const records = await response.json(); 
+            const resultLimit = 10000;
+            const totalResults = records.total_results > resultLimit? resultLimit: records.total_results;
+            const maxPage = Math.floor(totalResults / 200);
 
-      function parseNested(str) {
-          try {
-              return JSON.parse(str, (_, val) => {
-                  if (typeof val === 'string')
-                      return parseNested(val)
-                  return val
-              })
-          } catch (e) {
-              return str
-          }
-      }
+            if(!records) return {
+               page,
+               per_page: 200,
+               results: [],
+               total_results: 0 
+            }
 
-		// Note Need to Throttle to < 100 requests per minute as per iNaturalist API guidelines
-	  async function getInatProjectOccurrences(inat_proj_id, page = 1) {
-		  let url = `https://api.inaturalist.org/v1/observations?project_id=${inat_proj_id}&geo=true&page=${page}&mappable=true&per_page=200`;
-		  let response = await fetch(url, {
-			  method: "GET",
-			  headers: {
-				  "Content-Type": "application/json",
-			  }
-		  })
-		  const records = await response.json(); 
-		  const resultLimit = 10000;
-		  const totalResults = records.total_results > resultLimit? resultLimit: records.total_results;
-		  const maxPage = Math.floor(totalResults / 200);
-
-		  if(!records) return {
-			  page,
-			  per_page: 200,
-			  results: [],
-			  total_results: 0 
-		  }
-
-		  if(page !== maxPage) {
-			  //Wait a second and then make another api call for next page
-			  await new Promise(resolve => {
-				  setTimeout(resolve, 1000, false);
-			  })
-			  const nextResult = await getInatProjectOccurrences(inat_proj_id, page + 1)
-			  records.results = [...records.results, ...nextResult.results];
-		  }
-		  return records;
-	  }
-
-		function initialize() {
-         try {
-            let data = document.getElementById('service-container')
-            occurCoords = JSON.parse(data.getAttribute('data-occur-coords'));
-            clMeta = parseNested(data.getAttribute('data-cl-meta'));
-         } catch (err) {
-            alert("Failed to load occurence data")
+            if(page !== maxPage) {
+               //Wait a second and then make another api call for next page
+               await new Promise(resolve => {
+                  setTimeout(resolve, 1000, false);
+               })
+               const nextResult = await getInatProjectOccurrences(inat_proj_id, page + 1)
+               records.results = [...records.results, ...nextResult.results];
+            }
+            return records;
          }
 
-         if(clMeta && clMeta.footprintwkt) {
-            cl_footprint_shape = loadMapShape("polygon", { polygonLoader: () => clMeta.footprintwkt });
+         function initialize() {
+            try {
+               let data = document.getElementById('service-container')
+               occurCoords = JSON.parse(data.getAttribute('data-occur-coords'));
+               clMeta = parseNested(data.getAttribute('data-cl-meta'));
+            } catch (err) {
+               alert("<?php echo $LANG['FAILED_TO_LOAD'] ?>")
+            }
+
+            if(clMeta && clMeta.footprintwkt) {
+               cl_footprint_shape = loadMapShape("polygon", { polygonLoader: () => clMeta.footprintwkt });
+            }
+
+            <?php if(empty($GOOGLE_MAP_KEY)):?>
+               leafletInit();
+            <?php else:?>
+               googleInit();
+            <?php endif?>
          }
 
-         <?php if(empty($GOOGLE_MAP_KEY)):?>
-            leafletInit();
-         <?php else:?>
-            googleInit();
-         <?php endif?>
-      }
-
-		function openIndPU(occId){
-			if(puWin != null) puWin.close();
-			var puWin = window.open('../collections/individual/index.php?occid='+occId,'indspec' + occId,'scrollbars=1,toolbar=0,resizable=1,width=900,height=600,left=20,top=20');
-			if(puWin.opener == null) puWin.opener = self;
-			setTimeout(function () { puWin.focus(); }, 0.5);
-			return false;
-      }
-	</script>
-	<style>
-		html, body, #map_canvas {
-			width: 100%;
-			height: 100%;
-			margin: 0;
-			padding: 0;
-		}
-	</style>
-</head>
-<body style="background-color:#ffffff;" onload="initialize();">
-<?php
-	if(!$coordArr){
-		?>
-		<div style='font-size:120%;font-weight:bold;'>
-			<?php echo $LANG['NO_COORDS']; ?>.
-		</div>
-		<div style="margin:15px;">
-			<?php echo $LANG['MAYBE_RARE']; ?>.
-		</div>
-		<?php
-	}
-	?>
-   <div id="service-container"
-      data-occur-coords="<?= htmlspecialchars($coordJson) ?>"
-      data-cl-meta="<?= htmlspecialchars($metaJson)?>"
-   />
-	<div id='map_canvas'></div>
-</body>
+         function openIndPU(occId){
+            if(puWin != null) puWin.close();
+            var puWin = window.open('../collections/individual/index.php?occid='+occId,'indspec' + occId,'scrollbars=1,toolbar=0,resizable=1,width=900,height=600,left=20,top=20');
+            if(puWin.opener == null) puWin.opener = self;
+            setTimeout(function () { puWin.focus(); }, 0.5);
+            return false;
+         }
+      </script>
+      <style>
+         html, body, #map_canvas {
+            width: 100%;
+            height: 100%;
+            margin: 0;
+            padding: 0;
+         }
+      </style>
+   </head>
+   <body style="background-color:#ffffff;" onload="initialize();">
+      <?php
+         if(!$coordArr){
+            ?>
+            <h1 class="for-screen-reader"><?= $LANG['NO_COORDS_HEADER']; ?></h1>
+            <div style='font-size:120%;font-weight:bold;'>
+               <?php echo $LANG['NO_COORDS']; ?>.
+            </div>
+            <div style="margin:15px;">
+               <?php echo $LANG['MAYBE_RARE']; ?>.
+            </div>
+            <?php
+         }
+         ?>
+         <h1 class="for-screen-reader"><?= $LANG['MAP_SECTION']; ?></h1>
+         <div id="service-container"
+            data-occur-coords="<?= htmlspecialchars($coordJson) ?>"
+            data-cl-meta="<?= htmlspecialchars($metaJson)?>"
+         ></div>
+         <div id='map_canvas'></div>
+   </body>
 </html>
