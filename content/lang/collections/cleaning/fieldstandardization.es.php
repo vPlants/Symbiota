0361d75266cf26cf0962299a4a76f6036c8bd528
Ramisa Zaman <129628108+RamisaZaman@users.noreply.github.com>
2024-03-19 22:40:19 -0700

    Language Tags for collections/cleaning/fieldstandardization.php (#1030)

---
+++ content/lang/collections/cleaning/fieldstandardization.es.php
@@ -0,0 +1,21 @@
+<?php
+/*
+------------------
+Language: Español (Spanish)
+Translated by: Google Translate
+Date Translated: 2024/03/17
+------------------
+*/
+$LANG['FIELD_STANDARDIZATION'] = 'Estandarización de campos';
+$LANG['HOME'] = 'Inicio';
+$LANG['COLLECTION_MANAGEMENT'] = 'Gestión de colecciones';
+$LANG['BATCH_FIELD_TOOLS'] = 'Herramientas de limpieza de campos por lotes';
+$LANG['COUNTRY'] = 'País';
+$LANG['OLD_FIELD'] = 'Campo antiguo';
+$LANG['SELECT_TARGET_FIELD'] = 'Seleccionar campo de destino';
+$LANG['OLD_VALUE'] = 'Valor anterior';
+$LANG['SELECT_TARGET_VALUE'] = 'Seleccionar valor objetivo';
+$LANG['REPLACEMENT_VALUE'] = 'Valor de reemplazo';
+$LANG['NOT_AUTHORIZED'] = 'No estás autorizado a acceder a esta página';
+
+?>
