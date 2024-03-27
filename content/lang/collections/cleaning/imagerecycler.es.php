be084dbbe105ede3fbd10e237d37559d0739f91f
Ramisa Zaman <129628108+RamisaZaman@users.noreply.github.com>
2024-03-26 22:26:46 -0700

    Lang Tags for collections/cleaning/imagerecycler.php (#1070)

---
+++ content/lang/collections/cleaning/imagerecycler.es.php
@@ -0,0 +1,18 @@
+<?php
+/*
+------------------
+Language: Español (Spanish)
+Translated by: Google Translate
+Date Translated: 2024/03/23
+------------------
+*/
+$LANG['IMAGE_RECYCLER'] = 'Reciclador de imágenes';
+$LANG['HOMEPAGE'] = 'Página de inicio';
+$LANG['COL_MAN_MEN'] = 'Menú de gestión de colecciones';
+$LANG['BULK_IMAGE_RECYCLER'] = 'Reciclador de imágenes masivas';
+$LANG['BATCH_IMAGE_REMOVER'] = 'Eliminador de imágenes por lotes';
+$LANG['BATCH_IMAGE_DELETION_TOOL'] = 'Esta herramienta eliminará imágenes por lotes basándose en el envío de múltiples identificadores de imágenes.';
+$LANG['IMAGE_IDENTIFIERS'] = 'Identificadores de imagen';
+$LANG['BULK_REMOVE_IMAGE_FILES'] = 'Eliminación masiva de archivos de imagen';
+$LANG['ERROR_COLLECTION'] = 'ERROR: el identificador de colección no está establecido';
+?>
