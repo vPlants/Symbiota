8a6bc535947c64a5830f99d580f8134f62097279
Ramisa Zaman <129628108+RamisaZaman@users.noreply.github.com>
2024-03-27 14:53:37 -0700

    Lang Tags for imagelib\imgdetails.php (#1080)
    * Lang Tags for imagelib\imgdetails.php
    
    * Add spaces and 1 lang tag
    
    * Update imgdetails.en.php
    
    * Update imgdetails.es.php
    
    * Update imgdetails.fr.php
    
    * Update imgdetails.php
    
    * Fix French translation
    
    * Update capitalization
    
    * Update imgdetails.fr.php
    
    * Update imgdetails.es.php
    
    ---------
    
    Co-authored-by: Katie Pearson <katelin.d.pearson24@gmail.com>

---
+++ content/lang/imagelib/imgdetails.fr.php
@@ -0,0 +1,64 @@
+<?php
+/*
+------------------
+Language: Français (French)
+Translated by: Google Translate
+Date Translated: 2024/03/26
+------------------
+*/
+
+$LANG['ERROR_FILE_PATH'] = 'ERREUR : Le chemin du fichier doit être saisi';
+$LANG['ENTER_TAXON_NAME'] = 'Entrez un nom de taxon vers lequel l\'image sera transférée';
+$LANG['TAXON_PROFILE_EDITING'] = 'Aller à la page d\'édition du profil de taxon';
+$LANG['EDITING_PRIVILEGES'] = 'Doit disposer de privilèges d\'édition pour cette collection gérant l\'image';
+$LANG['EDIT_IMAGE'] = 'Modifier l\'image';
+$LANG['TWEET'] = 'Tweet';
+$LANG['EDIT_IMAGE_DETAILS'] = 'Modifier les détails de l\'image';
+$LANG['CAPTION'] = 'Légende';
+$LANG['PHOTOGRAPHER_USER_ID'] = 'ID du Photographe';
+$LANG['SELECT_PHOTOGRAPHER'] = 'Sélectionner un Photographe';
+$LANG['USER_REGISTERED_SYSTEM'] = 'Utilisateurs enregistrés dans le système';
+$LANG['DISPLAY_PHOTOGRAPHER_FIELD'] = 'Afficher le champ de remplacement du photographe';
+$LANG['DISPLAY_SPECIMEN_DETAILS'] = 'Afficher les détails du spécimen';
+$LANG['PHOTOGRAPHER_OVERRIDE'] = 'Photographe (remplacement)';
+$LANG['PHOTOGRAPHER'] = 'Photographe';
+$LANG['OVERRIDE_SELECTION'] = 'Ecrasera la sélection ci-dessus';
+$LANG['MANAGER'] ='Gestionnaire';
+$LANG['SOURCE_URL'] = 'URL Source';
+$LANG['COPYRIGHT'] = 'Droits d\'Auteur';
+$LANG['COPYRIGHT_DETAILS'] = 'Détails des droits d\'auteur';
+$LANG['RIGHTS'] = 'Droits';
+$LANG['LOCALITY'] = 'Localité';
+$LANG['OCCURRENCE_RECORD'] = 'Enregistrement d\'Occurrence';
+$LANG['LINK_OCCUR_RECORD'] = 'Lien vers l\'enregistrement d\'occurrence';
+$LANG['NOTES'] = 'Notes';
+$LANG['SORT_SEQUENCE'] = 'Séquence de Tri';
+$LANG['WEB_IMAGE'] = 'Image Web';
+$LANG['RENAME_WEB_IMAGE_FILE'] = 'Renommer le fichier image Web sur le serveur pour qu\'il corresponde à la modification ci-dessus (privilèges d\'édition de fichiers du serveur Web requis)';
+$LANG['THUMBNAIL'] = 'Vignette';
+$LANG['RENAME_THUMBNAIL_IMAGE_FILE'] = 'Renommer le fichier d\'image miniature sur le serveur pour qu\'il corresponde à la modification ci-dessus (privilèges d\'édition de fichiers du serveur Web requis)';
+$LANG['LARGE_IMAGE'] = 'Grande Image';
+$LANG['RENAME_LARGE_IMAGE_FILE'] = 'Renommer le fichier image volumineux sur le serveur pour qu\'il corresponde à la modification ci-dessus (privilèges d\'édition de fichiers du serveur Web requis)';
+$LANG['TRANSFER_IMAGE_TO_DIFF_NAME'] = 'Transférer l\'image vers un autre nom scientifique';
+$LANG['TRANSFER_TO_TAXON'] = 'Transfert vers le taxon';
+$LANG['DELETE_IMAGE_FROM_SERVER'] = 'Êtes-vous sûr de vouloir supprimer cette image ? Notez que l\'image physique sera supprimée du serveur si la case est cochée.';
+$LANG['AUTHORIZED_REMOVE_IMAGE'] = 'Autorisé à supprimer cette image';
+$LANG['REMOVE_IMG_FROM_SERVER'] = 'Supprimer l\'image du serveur';
+$LANG['BOX_CHECKED_IMG_DELETED'] = '(Remarque : si la case est cochée, l\'image sera définitivement supprimée du serveur, ainsi que de la base de données)';
+$LANG['CLICK_IMAGE'] = 'Cliquez sur l\'image pour l\'agrandir';
+$LANG['SCIENTIFIC_NAME'] = 'Nom scientifique';
+$LANG['ERROR_COMMENT_ABOUT_IMAGE'] = 'Voyez-vous une erreur ou avez-vous un commentaire sur cette image?';
+$LANG['IMG_NO'] = 'Image #';
+$LANG['SEND_EMAIL'] = 'Si oui, envoyez un e-mail à';
+$LANG['SPEC'] = 'SPÉC';
+$LANG['IMG'] = 'IMG';
+$LANG['TP'] = 'PT'; //stands for Taxon Profile
+$LANG['SUBMIT_IMAGE_EDITS'] = 'Soumettre Modifications d\'Image';
+$LANG['TRANSFER_IMAGE'] = 'Transférer l\'Image';
+$LANG['DELETE_IMAGE'] = 'Supprimer l\'Image';
+$LANG['IMAGE_SOURCE'] = 'Source de l\'Image';
+$LANG['OPEN_MEDIUM_SIZED_IMAGE'] = 'Ouvrir une image de taille moyenne';
+$LANG['OPEN_LARGE_IMAGE'] = 'Ouvrir une grande image';
+$LANG['UNABLE_TO_LOCATE'] = 'Impossible de localiser l\'image';
+
+?>
