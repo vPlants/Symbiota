821b4095950a5add4428facd97a0519318c2eebe
Mark <mark.aaron.fisher@gmail.com>
2024-04-23 08:40:34 -0700

    Fix default icon colors (#1160)
    * add svg for link image and assign its fill a color from variables.css
    
    * change color and make icon image something that responds to variables.css

--- collections/editor/includes/queryform.php
+++ collections/editor/includes/queryform.php
@@ -99,7 +99,12 @@ else{
 					</div>
 
 					<button type="button" class="icon-button float-right" onclick="copyQueryLink(event)" title="<?php echo $LANG['COPY_SEARCH']; ?>" aria-label="<?php echo $LANG['COPY_LINK']; ?>">
-						<img src="../../images/link.png" style="width:1.2em;margin-right:5px;" alt="Link icon. Copies the search terms as a link." /><?php echo $LANG['COPY_LINK']; ?>
+						<span style="display:flex; align-content: center;">
+								<svg alt="Link icon. Copies the search terms as a link." style="width:1.2em;margin-right:5px;" xmlns="http://www.w3.org/2000/svg" fill="var(--light-color)" height="24" viewBox="0 -960 960 960" width="24"><path d="M440-280H280q-83 0-141.5-58.5T80-480q0-83 58.5-141.5T280-680h160v80H280q-50 0-85 35t-35 85q0 50 35 85t85 35h160v80ZM320-440v-80h320v80H320Zm200 160v-80h160q50 0 85-35t35-85q0-50-35-85t-85-35H520v-80h160q83 0 141.5 58.5T880-480q0 83-58.5 141.5T680-280H520Z"/></svg>
+								<span style="align-content: center;">
+									<?php echo $LANG['COPY_LINK']; ?>
+								</span>
+						</span>
 					</button>
 				</div>
 				<?php
