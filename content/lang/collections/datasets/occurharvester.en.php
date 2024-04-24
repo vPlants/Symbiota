9861a5e44c08d44a4e17c7be9cc04dd19080fe77
Ramisa Zaman <129628108+RamisaZaman@users.noreply.github.com>
2024-04-24 12:50:19 -0700

    Lang Tags for collections\datasets\occurharvester.php (#1132)
    * Lang Tags for collections\datasets\occurharvester.php
    
    * Date added
    
    * Fixed the tags

---
+++ content/lang/collections/datasets/occurharvester.en.php
@@ -0,0 +1,21 @@
+<?php
+/*
+------------------
+Language: English
+------------------
+*/
+
+$LANG['OCCUR_HARV'] = 'Occurrence Harvester';
+$LANG['UNABLE_TO_LOCATE'] = 'unable to locate occurrence record';
+$LANG['HOME'] = 'Home';
+$LANG['BARCODE_INPUT_INSTRUCTIONS'] = 'Scan or type barcode number into field below and then hit enter or tab to add the specimen to the list.
+			Once list is complete, you can enter your catalog number in the text field and then transfer to your collection
+			or file export to a file that can be imported into your local database.';
+$LANG['SPEC_QUEUE'] = 'Specimen Queue';
+$LANG['LIST_EMPTY'] = 'List Empty';
+$LANG['TRANSFER_RECORD'] = 'Transfer Records';
+$LANG['DOWNLOAD_RECORDS'] = 'Download Records';
+$LANG['OCCUR_ID'] = 'occid (Symbiota ID)';
+$LANG['ENTER_OCC_ID'] = 'enter occid (Symbiota ID) into the occid field';
+
+?>
