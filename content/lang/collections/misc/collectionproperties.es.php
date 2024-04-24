afb7e6dfa1d927f0109c1008d39d926945a3371d
Ramisa Zaman <129628108+RamisaZaman@users.noreply.github.com>
2024-04-24 13:00:40 -0700

    Lang Tags for collections\misc\collectionproperties.php (#1148)
    * Lang Tags for collections\misc\collectionproperties.php
    
    * Fixed Double Heading
    
    Suggested by Mark
    
    * Removed lang tags from future reference
    
    * Fixed Merge Conflicts

---
+++ content/lang/collections/misc/collectionproperties.es.php
@@ -0,0 +1,26 @@
+<?php
+/*
+------------------
+Language: Español (Spanish)
+Translated by: Google Translate
+Date Translated: 2024/04/16
+------------------
+*/
+
+$LANG['PROFILE_UPDATE_SUCCESS'] = '¡Éxito! Perfil de colección antiguo actualizado';
+$LANG['UPDATING_COLLEC_PROFILE'] = 'actualizando perfil de colección';
+$LANG['ERROR'] = 'ERROR';
+$LANG['SELECT_USER'] = 'Por favor seleccione un usuario de la lista';
+$LANG['MANAGEMENT_PROP'] = 'Propiedades de Gestión';
+$LANG['COLLEC_MANAGEMENT_PROPERTIES'] = 'Propiedades de Gestión de Colecciones';
+$LANG['WARNING'] = 'ADVERTENCIA';
+$LANG['COLLEC_PROP_NOT_ACTIVATED'] = 'Las Propiedades de la Colección aún no se han activado (por ejemplo, faltan tablas u otros problemas de esquema)';
+$LANG['OLD_COLLEC_PROPERTIES'] = 'Las Propiedades de la Colección antigua deben convertirse al nuevo formato';
+$LANG['CONVERT_TO_NEW_FORMAT'] = 'Convertir al nuevo formato';
+$LANG['ACTION_PANEL'] = 'Panel de Acciones';
+$LANG['OVERRIDE_COLLECTION_TITLE'] = 'El siguiente campo anulará el título del proyecto de colección/observación que se publica en el archivo EML dentro del archivo de exportación Darwin Core Archive (DwC-A)';
+$LANG['SAVE_TITLE_OVERRIDE'] = 'Guardar Anulación de Título';
+
+
+?>
+
