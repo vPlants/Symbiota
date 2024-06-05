be64c2c34429ca418428532dc0f41c1a7abb2202
Mark <mark.aaron.fisher@gmail.com>
2024-06-04 18:57:14 -0700

    [3.1] - select collections from the url if present (#1400)
    * select collections from the url if present
    
    * DRY up sanitizedCollectionSource section, which has the same pattern as occurs elsewhere
    
    * fix typo
    
    * change default to empty array
    
    * add better type checking to collIdsFromUrl

--- collections/search/index.php
+++ collections/search/index.php
@@ -12,6 +12,8 @@ header("Content-Type: text/html; charset=" . $CHARSET);
 if($LANG_TAG != 'en' && file_exists($SERVER_ROOT.'/content/lang/collections/search/index.' . $LANG_TAG . '.php')) include_once($SERVER_ROOT.'/content/lang/collections/search/index.' . $LANG_TAG . '.php');
 else include_once($SERVER_ROOT . '/content/lang/collections/search/index.en.php');
 
+$collIdsFromUrl = array_key_exists("db",$_GET) ? explode(",", str_replace(array('[',']'), '', $_GET["db"])) : '';
+
 $collManager = new OccurrenceManager();
 $collectionSource = $collManager->getQueryTermStr();
 
@@ -567,31 +569,15 @@ $obsArr = (isset($collList['obs'])?$collList['obs']:null);
 		ul.outerWidth(this.element.outerWidth());
 	}
 	const collectionSource = <?php echo isset($collectionSource) ? json_encode($collectionSource) : 'null'; ?>;
+	const collIdsFromUrl = <?php echo isset($collIdsFromUrl) ? json_encode($collIdsFromUrl) : 'null'; ?>;
+	if (collIdsFromUrl && Array.isArray(collIdsFromUrl) && collIdsFromUrl.length > 0) {
+		uncheckEverything();
+		checkTheCollectionsThatShouldBeChecked(collIdsFromUrl);
+	}
 	const sanitizedCollectionSource = collectionSource.replace('db=','');
-
-	if(collectionSource){
-		// go through all collections and set them all to unchecked
-		const collectionCheckboxes = document.querySelectorAll('input[id^="coll"]');
-		collectionCheckboxes.forEach(collection => {
-			collection.checked = false;
-		});
-
-		//go through all collections and set the parent collections to unchecked
-		const parentCollectionCheckboxes = document.querySelectorAll('input[id^="cat-"]');
-		parentCollectionCheckboxes.forEach(collection => {
-			collection.checked = false;
-		});
-
-		// set the one with collectionSource as checked
-		const targetCheckbox = document.querySelectorAll('input[id^="coll-' + sanitizedCollectionSource + '"]');
-		targetCheckbox.forEach(collection => {
-			collection.checked = true;
-		});
-		//do the same for collections with slightly different format
-		const targetCheckboxAlt = document.querySelectorAll('input[id^="collection-' + sanitizedCollectionSource + '"]');
-		targetCheckboxAlt.forEach(collection => {
-			collection.checked = true;
-		});
+	if (collectionSource) {
+		uncheckEverything();
+		checkTheCollectionsThatShouldBeChecked(sanitizedCollectionSource);
 		updateChip();
 	}
 
