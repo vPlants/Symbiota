0178315a0f341788f9ca84ba02398541863bc684
Logan Wilt <91149982+MuchQuak@users.noreply.github.com>
2024-07-19 13:30:42 -0700

    Occurrence Query String Multiple taxa fix (#1520)

--- classes/OccurrenceManager.php
+++ classes/OccurrenceManager.php
@@ -637,7 +637,9 @@ class OccurrenceManager extends OccurrenceTaxaManager {
 			if($v) $retStr .= '&'. $this->cleanOutStr($k) . '=' . $this->cleanOutStr($v);
 		}
 		if(isset($this->taxaArr['search'])){
-			$patternOfOnlyLettersDigitsAndSpaces = '/^[a-zA-Z0-9\s\-]*$/'; // TOOD accommodate symbols associated with extinct taxa, hybrid crosses, and abbreviations with periods, e.g. "var."?
+			$patternOfOnlyLettersDigitsAndSpaces = '/^[a-zA-Z0-9\s\-\,]*$/'; // TOOD accommodate symbols associated with extinct taxa, hybrid crosses, and abbreviations with periods, e.g. "var."?
+
+
 			if (preg_match($patternOfOnlyLettersDigitsAndSpaces, $this->getTaxaSearchTerm())==1) {
 				$retStr .= '&taxa=' . $this->getTaxaSearchTerm();
 			}
