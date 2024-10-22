59067170f2278a92d0c6d2e8608bec3842f3a949
Katie Pearson <katelin.d.pearson24@gmail.com>
2024-10-22 06:47:04 -0700

    Finish lang tags for editor/rpc (#1793)
    * Finish lang tags for editor/rpc
    
    - Combined all the lang tags in this folder into a single lang tag file (short)
    
    * Add lang tags to editor/tools

--- content/lang/collections/editor/rpc/editor_rpc.es.php
+++ content/lang/collections/editor/rpc/editor_rpc.es.php
@@ -3,11 +3,16 @@
 ------------------
 Language: Español (Spanish)
 Translated by: Google Translate
-Date Translated: 2024/02/12
+Date Translated: 2024-10-17
 ------------------
 */
 
-$LANG['DUPLICATE_LINKER'] = 'Enlazador duplicado';
+//dupedelete.php
+$LANG['ERROR_1'] = 'ERROR desconocido [1]';
+$LANG['ERROR_2'] = 'ERROR desconocido [2]';
+
+//dupelist.php
+$LANG['DUPLICATE_LINKER'] = 'Enlazador de duplicados';
 $LANG['LINK_NEW_SPECIMEN'] = 'Enlazar nuevo espécimen';
 $LANG['LAST_NAME'] = 'Apellido:';
 $LANG['NUMBER'] = 'Número:';
@@ -19,5 +24,9 @@ $LANG['POSSIBLE_DUPLICATES'] = 'Posibles duplicados';
 $LANG['MORE_DETAILS'] = 'Más detalles';
 $LANG['LINK_AS_DUPLICATE'] = 'Enlace como duplicado';
 $LANG['NO_SPECIMENS_FOUND'] = 'No se encontraron especímenes que coincidan con los criterios de búsqueda';
+
+//makeactionrequest.php
+$LANG['FAILED_ADD_REQUEST'] = 'Error al agregar la solicitud';
+$LANG['ADDED_REQUEST'] = 'Solicitud agregada para';
 
 ?>
