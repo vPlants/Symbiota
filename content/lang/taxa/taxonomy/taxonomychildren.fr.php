58b19f55b6a8463a1031b0c872fbde6bf893b3ba
Katie Pearson <katelin.d.pearson24@gmail.com>
2024-10-28 10:00:53 -0700

    Add French lang tags for taxonomy files (#1791)
    * Add batchloader and taxoneditor lang tags in fr
    
    * Add more French translations

---
+++ content/lang/taxa/taxonomy/taxonomychildren.fr.php
@@ -0,0 +1,14 @@
+<?php
+/*
+------------------
+Language: Français (French)
+Translated by: Google Translate
+Translation date: 2024-10-16
+------------------
+*/
+
+$LANG['CHILDREN_TAXA'] = 'Taxons enfants';
+$LANG['SHOWING_DIRECT'] = 'Affichage des enfants directs uniquement';
+$LANG['NO_CHILDREN'] = "Aucun taxon enfant n'existe pour ce taxon";
+
+?>
