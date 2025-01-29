79755565309fa869ac5f65aab20dbee51212d1e0
Logan Wilt <91149982+MuchQuak@users.noreply.github.com>
2025-01-29 09:53:24 -0800

    Closes #2119 Fixes Media Form History (#2148)
    * Closes #2119 Fixes Media Form History
    
    # Issue #2119
    
    # Summary
    After search variables that were previously selected were not being
    overidden by changes in the form. This was do to altering the form
    action in order to preserve the url after a post. Fix was to load this
    with js so that the history is correct but does not pollute the next
    request.
    
    * cleaning up logic

--- imagelib/search.php
+++ imagelib/search.php
@@ -62,6 +62,7 @@ if($action == 'batchAssignTag'){
 		$statusStr = '<span style="color:red">' . $LANG['ACTION_ERROR'] . ': ' . $imgLibManager->getErrorStr() . '</span>';
 	}
 }
+
 ?>
 <!DOCTYPE html>
 <html lang="<?= $LANG_TAG ?>">
@@ -132,7 +133,7 @@ if($action == 'batchAssignTag'){
 	<!-- This is inner text! -->
 	<div role="main" id="innertext">
 		<h1 class="page-heading"><?= $LANG['IMAGE_SEARCH']; ?></h1>
-		<form name="imagesearchform" id="imagesearchform" action="search.php?<?=$imgLibManager->getQueryTermStr()?>" method="post">
+		<form name="imagesearchform" id="imagesearchform" action="search.php" method="post">
 			<?php
 			if($statusStr){
 				echo '<div id="action-status-div">' . $statusStr . '</div>';
@@ -504,6 +505,9 @@ if($action == 'batchAssignTag'){
 			}
 			?>
 		</form>
+		<script type ="text/javascript">
+			history.replaceState({},'', "?<?= $imgLibManager->getQueryTermStr() ?>");
+		</script>
 	</div>
 	<?php
 	include($SERVER_ROOT . '/includes/footer.php');
