55faae27ed994c3faecc569e1a2486e4e0d1a40c
NikitaSalikov <86389284+NikitaSalikov@users.noreply.github.com>
2025-03-21 09:11:27 -0700

    Taxon loader form fix + translations (#2305)
    * added a string length check, added erroneous input checks, added translations
    
    * remove the console log
    
    * added missing translations
    
    * fix spanish translation
    
    * added translations for button
    
    * added translations for the header

--- js/symb/es.js
+++ js/symb/es.js
@@ -1,3 +1,21 @@
 const translations = {
   CLICK_TO_DISMISS: "Haga clic para descartar",
+  // taxonomyloader:
+  BUTTON_SUBMIT: "Enviar nuevo nombre",
+  BUTTON_CHECKING: "Comprobando errores...",
+  BUTTON_DISABLED: "Botón deshabilitado",
+  TAXON: "Taxón",
+  PARENT_TAXON: "Taxón padre",
+  FIELD_TOO_LONG: "El texto del campo es demasiado largo. El límite de caracteres para este campo es",
+  PARENT_TAXON_REQUIRED: "Taxón Superior requerido.",
+  PARENT_ID_NOT_SET: "¡El identificador principal no está configurado! Asegúrese de seleccionar el taxón principal de la lista.",
+  ACC_NAME_NEEDS_VALUE: "El nombre aceptado debe tener un valor.",
+  SCI_NAME_RANK_REQUIRED: "Nombre/Rango de taxón requerido.",
+  ALREADY_EXISTS: "Ya existe en la base de datos.",
+  UNIT_NAME_REQUIRED: "Campo de nombre de unidad 1 (género o uninomial) requerido.",
+  TAXON_RANK_REQUIRED: "Rango de Taxón requerido.",
+  SELECT_ACCEPTED_NAME: "Debe seleccionar un nombre de la lista. Si el nombre aceptado no está en la lista, debe agregarse o se encuentra en el sistema como un sinónimo no aceptado",
+  SELECT_PARENT_NAME: "Debe seleccionar un nombre de la lista. Si Taxón Superior no está en la lista, podría ser necesario agregarlo",
+  TAXON_NOT_EXISTS: "No existe. Por favor, agregue primero el padre al sistema.",
+  MATCHES_TWO: "Coincide con dos nombres diferentes en el tesauro. Por favor, seleccione el taxón con el autor correcto.",
 };
