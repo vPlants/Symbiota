2219748cc8ecc5bb1020567b5810b8c2224ac3f4
Edward Gilbert <egbot@asu.edu>
2025-04-09 09:16:44 -0700

    3.3 db schema patch (#2380)
    * 3.3 db schema patch 
    
    - Full-record security schema adjustments
    - Schema adjustments needed to synchronize temp upload staging tables with core target tables
    - Portal Index field additions
    - key-value upload staging table indexing adjustments to speed up linkages to existing records

--- config/schema/3.0/patches/db_schema_patch-3.3-portalindex.sql
+++
@@ -1,8 +0,0 @@
-
-
-ALTER TABLE `portalindex` 
-  ADD COLUMN `lastContact` TIMESTAMP NULL AFTER `notes`,
-  ADD COLUMN `modifiedTimestamp` TIMESTAMP NULL AFTER `lastContact`,
-  ADD COLUMN `apiVersion` VARCHAR(45) NULL AFTER `symbiotaVersion`;
-
-
